15ce4ab3e8498fdb341dbaa6fa96716df9e90cfd
mandalavideo <carleslopezt@gmail.com>
2018-08-20 13:34:49 +0200

    Update portfolio.js

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -187,7 +187,7 @@ var $contactForm = $('#contact-form');
 $contactForm.submit(function(e) {
 	e.preventDefault();
 	$.ajax({
-		url: '//formspree.io/info@mandalavideo.com',
+		url: 'https://formspree.io/info@mandalavideo.com',
 		method: 'POST',
 		data: $(this).serialize(),
 		dataType: 'json',
